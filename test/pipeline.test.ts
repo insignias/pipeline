4d7c9a63168405f6821899e7269b44d939184a37
Sharan <issharan@88665a345be3.ant.amazon.com>
2021-11-11 00:54:56 -0500

    Some fixes

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -2,10 +2,10 @@ import * as cdk from 'aws-cdk-lib';
 import * as Pipeline from '../lib/pipeline-stack';
 import { SynthUtils} from '@aws-cdk/assert';
 
-test('Empty Stack', () => {
+test('PipelineStack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
+    const stack = new Pipeline.PipelineStack(app, 'PipelineStack');
     // THEN
     expect(SynthUtils.toCloudFormation).toMatchSnapshot();
 });
