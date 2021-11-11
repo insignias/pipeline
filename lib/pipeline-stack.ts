3e638d33a52fc1ac8115a2a643f674d51433c6bc
Sharan <issharan@88665a345be3.ant.amazon.com>
2021-11-11 00:22:47 -0500

    Initial commit

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,10 +1,71 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { SecretValue, Stack, StackProps } from 'aws-cdk-lib';
+import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/lib/aws-codebuild';
+import { Artifact, Pipeline } from 'aws-cdk-lib/lib/aws-codepipeline';
+import { CloudFormationCreateUpdateStackAction, CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/lib/aws-codepipeline-actions';
 import { Construct } from 'constructs';
 
 export class PipelineStack extends Stack {
+  private pipeline: Pipeline;
+  private cdkBuildOutput: Artifact;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.pipeline = new Pipeline(this, "Pipeline", {
+      pipelineName: "Pipeline",
+      crossAccountKeys: true,
+      restartExecutionOnUpdate: true,
+    });
+
+    const cdkSourceOutput = new Artifact("CDKSourceOutput");
+
+    this.pipeline.addStage({
+      stageName: "Source",
+      actions: [
+        new GitHubSourceAction({
+          owner: "Gtofig",
+          repo: "aws-pipeline",
+          branch: "master",
+          actionName: "Pipeline_Source",
+          oauthToken: SecretValue.secretsManager("github-token"),
+          output: cdkSourceOutput,
+        })
+      ]
+    });
+
+    this.cdkBuildOutput = new Artifact("CdkBuildOutput");
+
+    this.pipeline.addStage({
+      stageName: "Build",
+      actions: [
+        new CodeBuildAction({
+          actionName: "CDK_Build",
+          input: cdkSourceOutput,
+          outputs: [this.cdkBuildOutput],
+          project: new PipelineProject(this, "CdkBuildProject", {
+            environment: {
+              buildImage: LinuxBuildImage.STANDARD_5_0,
+            },
+            buildSpec: BuildSpec.fromSourceFilename(
+              "build-specs/cdk-build-spec.yml"
+            ),
+          }),
+        })
+      ]
+    });
+
+    this.pipeline.addStage({
+      stageName: "Pipeline_Update",
+      actions: [
+        new CloudFormationCreateUpdateStackAction({
+          actionName: "Pipeline_Update",
+          stackName: "PipelineStack",
+          templatePath: this.cdkBuildOutput.atPath(
+            "PipelineStack.template.json"
+          ),
+          adminPermissions: true,
+        }),
+      ],
+    });
   }
 }
